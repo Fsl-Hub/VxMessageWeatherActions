03ad5ad34813fb9060b56308ae7ee698394f1a5f
Fsl-Hub <112750268+Fsl-Hub@users.noreply.github.com>
2022-10-10 09:29:30 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -80,17 +80,6 @@ public class AllConfig {
                 new BirthDay(2020,7,8,true,false)
         ));
 
-        userList.add(getUser(
-                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID",//要给这个人发送的模板ID
-                "这个人的称呼",//咋称呼这个人
-                "江苏省南京市玄武区",//这个人的详细地址
-                "南京",//这个人在的城市
-                new BirthDay(1999,8,11,false,false),
-                new BirthDay(1999,2,15,true,false),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false)
-        ));
 
 
 
