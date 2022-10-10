b57f9a3af045c2a24dfcd3ca3c17ad8eb438813d
Fsl-Hub <112750268+Fsl-Hub@users.noreply.github.com>
2022-10-10 12:03:29 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -69,9 +69,9 @@ public class AllConfig {
         //  注意：日期里面的数字，填正常的数字就行了.比如1就是1，不要填01
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         userList.add(getUser(
-                "o7s5v59UprE9p6K2WSDjXYtKx0uU", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "o7s5v5zmeujIaZ07JZvxT37paO3Q", //扫码关注你的测试号以后，测试平台会出现TA的微信号
                 "kbO_XqsTmy6G2EbLdaMfj4xjFycZfPO8pCRJ1ufYb3Y", //要给这个人发送的模板ID
-                "臭丫头", //咋称呼这个人
+                "亲爱的臭丫头", //咋称呼这个人
                 "河南省新乡市原阳县祝楼乡平原新区", //这个人的详细地址
                 "新乡", //这个人在的城市
                 new BirthDay(1997,11,13,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
